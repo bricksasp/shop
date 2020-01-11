2fc5c9014a3b5b87f198947085ee726a91af938b
t yy <649909457@qq.com>
2020-01-11 11:36:50 +0800

    接口修改

--- controllers/SettingController.php
+++ controllers/SettingController.php
@@ -21,6 +21,8 @@ class SettingController extends BaseController {
         if ($data = Setting::getSetting($this->dataOwnerUid(), 'shop_')) {
         	$data['logoItem'] = $data['shop_logo'] ? Tools::format_array(File::find()->select(['id','file_url'])->where(['id' => $data['shop_logo']])->one(), ['file_url' => ['implode', ['', [Config::instance()->web_url, '###']], 'array']]) : (object)[];
         	$data['defaultImgItem'] = $data['shop_default_img'] ? Tools::format_array(File::find()->select(['id','file_url'])->where(['id' => $data['shop_default_img']])->one(), ['file_url' => ['implode', ['', [Config::instance()->web_url, '###']], 'array']]) : (object)[];
+            $wx = Setting::getSetting($this->dataOwnerUid(), 'wx_');
+            $data = array_merge($data, $wx);
         }
 		return $this->success($data ? $data : (object) []);
 	}
